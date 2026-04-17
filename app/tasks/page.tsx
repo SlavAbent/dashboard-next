cf734b7bcc3e9395ea067c709242a6c578f58c2a
@abentss <abent@softlab.ru>
2026-04-17 16:53:45 +0500

    fix db.json, fix app router

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,15 +1,3 @@
-import Aside from '@/entities/aside/ui';
-import Header from '@/widgets/header';
-
 export default function Tasks() {
-  return (
-    <div className="flex h-screen max-w-full">
-      <Aside />
-      <div className="flex w-full flex-col">
-        <Header />
-      </div>
-      <div></div>
-      {/*<Tasks />*/}
-    </div>
-  );
+  return <div>tasks</div>;
 }
