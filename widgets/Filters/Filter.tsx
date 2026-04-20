95fc4c46cc3e9519803f748689885f26a549770d
@abentss <abent@softlab.ru>
2026-04-20 16:46:41 +0500

    add click to current filter

--- widgets/Filters/Filter.tsx
+++ widgets/Filters/Filter.tsx
@@ -4,17 +4,35 @@ import { cn } from '@/lib/utils';
 import { TypographyP } from '@/shared/ui/Typography/TypographyP';
 import { FilterType } from '@/widgets/Filters/types';
 
-const Filter = ({ filter }: { filter: FilterType }) => {
+const Filter = ({
+  filter,
+  isActive,
+  handleFilterClick,
+}: {
+  filter: FilterType;
+  isActive: boolean;
+  handleFilterClick: (id: number) => void;
+}) => {
   return (
-    <div className="flex cursor-pointer items-center gap-1 py-5">
+    <div
+      className="flex cursor-pointer items-center gap-1 py-5"
+      onClick={() => handleFilterClick(Number(filter.id))}>
       <div>
         <SvgIcon
           icon={filter.icon}
-          className={cn('transition-colors duration-300')}
+          className={cn(
+            'transition-colors duration-300',
+            isActive ? 'text-black' : 'text-[#727272] group-hover:text-black'
+          )}
         />
       </div>
 
-      <TypographyP text={filter.name} />
+      <TypographyP
+        text={filter.name}
+        className={`${
+          isActive ? 'text-black' : 'text-neutral-80 group-hover:text-black'
+        }`}
+      />
     </div>
   );
 };
--- widgets/Filters/FiltersCreate.tsx
+++ widgets/Filters/FiltersCreate.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import { TypographyH3 } from '@/shared/ui/Typography/TypographyH3';
 import { FiltersCreateType, FilterType } from '@/widgets/Filters/types';
@@ -8,16 +8,36 @@ import Filter from '@/widgets/Filters/Filter';
 
 const Filters = ({ data }: FiltersCreateType) => {
   const pathname = usePathname();
-  const pageTitle = pathname.split('/')[1];
-  const capitalizedTitle = `${pageTitle[0].toUpperCase()}${pageTitle.slice(1)}`;
+  const [isActiveFilter, setActiveFilter] = useState<number | null>(2);
+
+  const formatTitle = (pathname: string): string => {
+    const segment = pathname.split('/').filter(Boolean)[0];
+
+    if (!segment) return '/';
+
+    return `${segment.charAt(0).toUpperCase()}${segment.slice(1)}`;
+  };
+
+  const title = useMemo(() => formatTitle(pathname), [pathname]);
+
+  const handleFilterClick = useCallback((id: number) => {
+    setActiveFilter(id);
+  }, []);
 
   return (
     <div className="border-bottom flex items-center px-8">
-      <TypographyH3 text={capitalizedTitle} className="mr-9" />
+      <TypographyH3 text={title} className="mr-9" />
 
       <div className="flex items-center gap-5">
         {data.tasks.map((filter: FilterType) => {
-          return <Filter key={filter.id} filter={filter} />;
+          return (
+            <Filter
+              key={filter.id}
+              filter={filter}
+              isActive={isActiveFilter === filter.id}
+              handleFilterClick={handleFilterClick}
+            />
+          );
         })}
       </div>
       {/*<div>*/}
