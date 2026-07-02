60469d254f87b1e814a5787dd371153b63cb7baf
@abentss <abent@softlab.ru>
2026-07-02 13:17:30 +0500

    add info in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
+import { TypographyH3 } from '@/shared/components/Typography/TypographyH3';
+import { TypographySmall } from '@/shared/components/Typography/TypographySmall';
+import { WelcomeIcon } from '@/shared/icons/ui/Welcome';
+
 export default function Home() {
-  return <div>welcome to dashboard - next</div>;
+  return (
+    <div className="flex h-full items-center justify-center">
+      <div className="flex flex-col gap-20">
+        <WelcomeIcon />
+
+        <div className="flex flex-col items-center gap-4">
+          <TypographyH3 text="Welcome to Dashboard Next!" />
+          <TypographySmall text="Your data, time, and personnel management service" />
+        </div>
+      </div>
+    </div>
+  );
 }
